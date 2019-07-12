2293aa149af89451c94c3d75979f1fd14b39ad8a
PerS21 <perepel651@mail.ru>
2019-07-12 16:13:56 +0300

    рефакторинг
    рефакторинг

--- deepEqual.js
+++ deepEqual.js
@@ -16,15 +16,39 @@ let b = {
 
 
 console.log(a === b);
-let res;
 
 
-function deepEqual(a, b) {
-    res = (a.name !== b.name) ? false : (a.order.price === b.order.price) ? true : false
-    console.log(res)
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) {
+        return true;
+    }
+    if (obj1 === null || obj2 === null) {
+        return false
+    }
+    if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+        return false
+    }
+
+    let keysObj1 = Object.keys(obj1);
+    let keysObj2 = Object.keys(obj2);
+
+    if(keysObj1.length !== keysObj2.length){
+        return false;
+    }
+
+    for (let keyOfObj1 of keysObj1) {
+       
+        if (!deepEqual(obj1[keyOfObj1], obj2[keyOfObj1])) {
+            return false;
+        }
+    }
+    return keysObj1.every((key)=>deepEqual(obj1[key], obj2[key]));
+
 }
 
-console.log(res)
+console.log(deepEqual(a, b))
 
-deepEqual(a, b);
-console.log(res)
+// console.log(res)
+
+// deepEqual(a, b);
+// console.log(res)
