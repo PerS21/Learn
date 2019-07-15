ca2df6776b5ef1e961e3d9df12b84ab47309619e
PerS21 <perepel651@mail.ru>
2019-07-15 19:33:40 +0300

    смена цвета у ссылок

--- backgroundCollor/backgroundCollor.js
+++ backgroundCollor/backgroundCollor.js
@@ -6,7 +6,7 @@ for (let i = 0; i < arr.length; i++) {
     let href = arr[i].href
     if (href.startsWith('http://internal.com/test')) {} else if ((href.startsWith('http://') || (href.startsWith('ftp://')))) {
         console.log(i)
-        document.querySelector("ul").children[i].setAttribute('class', 'external')
+        document.querySelector("ul").children[i].firstElementChild.setAttribute('class', 'external')
     }
 }
 // let b = document.querySelector('ul')
