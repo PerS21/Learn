2cbc1182cfe210fda8f2085c45a2452ca62e76d4
PerS21 <perepel651@mail.ru>
2019-07-21 20:31:29 +0300

    добавил кнопки для выполнения

--- workWithTable/workWithTable.js
+++ workWithTable/workWithTable.js
@@ -1,46 +1,64 @@
 'use strict';
 
-function highlight(table) {
-//   console.log(table);
-  
+
+let table = document.querySelector('.js-teachers')
 let tbody = table.querySelector('tbody')
-// console.log(tbody)
 let trs = tbody.getElementsByTagName('tr');
-// console.log(trs[1].querySelector('td[data-available]'))
 
 
-let available =  document.querySelector('.js-teachers').querySelector('tbody').querySelector('tr:nth-child(1)').querySelector('td[data-available]').getAttribute('data-available')
-// console.log(available)
-
-for (let i = 0; i < trs.length; i++) {
-    if (!trs[i].querySelector('td[data-available]')) {
-        trs[i].setAttribute('class','hidden')
-    } else {
-    let a = (trs[i].querySelector('td[data-available]').getAttribute('data-available'))
-    // console.log(a)
-    if (a === "true"){
-        trs[i].setAttribute('class','available')
-    }else trs[i].setAttribute('class','unavailable')
-    }
-}
-//  document.querySelector('.js-teachers').querySelector('tbody').querySelector('tr:nth-child(1)').querySelector('td:nth-child(2)').innerText
-// for (let i = 0; i < trs.length; i++) {
-//     let gender = trs[i].querySelector('td:nth-child(3)').innerText
-//     // console.log(gender)
-//     if (gender === 'm') {
-//         trs[i].setAttribute('class','male')
-//     } else trs[i].setAttribute('class','female')
-    
-// }
-
-for (let i = 0; i < trs.length; i++) {
-    let age = trs[i].querySelector('td:nth-child(2)').innerText
-    if (age < 18) {
-        trs[i].setAttribute('class','junior')
+function handler(event) {
+    for (let i = 0; i < trs.length; i++) {
+        if (!trs[i].querySelector('td[data-available]')) {
+            trs[i].setAttribute('class','hidden')
+        } else {
+        let a = (trs[i].querySelector('td[data-available]').getAttribute('data-available'))
+        // console.log(a)
+        if (a === "true"){
+            trs[i].setAttribute('class','available')
+        }else trs[i].setAttribute('class','unavailable')
+        }
     }
 }
 
-//   console.log(trs);
+function handler2(event) {
+    for (let i = 0; i < trs.length; i++) {
+    let gender = trs[i].querySelector('td:nth-child(3)').innerText
+    console.log(gender)
+    if (gender === 'm') {
+        trs[i].setAttribute('class','male')
+    } else trs[i].setAttribute('class','female') 
+}
 }
 
-highlight(document.querySelector('.js-teachers')); 
+function handler3(event) {
+    for (let i = 0; i < trs.length; i++) {
+        let age = trs[i].querySelector('td:nth-child(2)').innerText
+        if (age < 18) {
+            trs[i].setAttribute('class','junior')
+        }
+    }
+}
+
+function handler4(event) {
+    for (let i = 0; i < trs.length; i++) {
+
+            trs[i].setAttribute('class','null')
+        
+    }
+}
+
+const button1 = document.querySelectorAll('button')[0];
+console.log(button1)
+button1.addEventListener('click',handler);
+
+const button2 = document.querySelectorAll('button')[1];
+console.log(button2)
+button2.addEventListener('click',handler2);
+
+const button3 = document.querySelectorAll('button')[2];
+console.log(button3)
+button3.addEventListener('click',handler3);
+
+const button4 = document.querySelectorAll('button')[3];
+console.log(button4)
+button4.addEventListener('click',handler4);
