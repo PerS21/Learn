8243efa31328d01b8f82d97b04fb9f3f94621b29
PerS21 <perepel651@mail.ru>
2019-07-24 22:00:52 +0300

    уход от привязанности к значениям

--- moveBallField/moveBallField.js
+++ moveBallField/moveBallField.js
@@ -11,25 +11,49 @@ const field = document.querySelector('div')
 field.addEventListener('click', handler);
 
 function handler(event) {
-    // console.log(event.clientX)
-    // console.log(event.clientY)
-    ball.style.top = (event.clientY - 56) + 'px'
-    ball.style.left = (event.clientX - 18) + 'px'
+    console.log(event.clientX)
+    console.log(event.clientY)
+    // ball.style.top = (event.clientY - 56 - (ball.clientHeight / 2)) + 'px'
+    // ball.style.left = (event.clientX - 18 - (ball.clientHeight / 2)) + 'px'
 
-    if (event.clientX > 200) {
-        ball.style.left = 200 - 20 + 'px'
+    // let a = field.getBoundingClientRect().top - field.clientTop
+    // let b = field.getBoundingClientRect().left - field.clientLeft
+    // console.log(a,b)
+
+    // if (event.clientX > 200) {
+    //     ball.style.left = 200 + 'px'
+    // }
+
+    // if (event.clientX < 38) {
+    //     ball.style.left = 25 + 'px'
+    // }
+
+
+    // if (event.clientY > 186) {
+    //     ball.style.top = 206 - 56 + 'px'
+    // }
+
+    // if (event.clientY < 78) {
+    //     ball.style.top = 0 + 'px'
+    // }
+
+    ball.style.top = (event.clientY - field.getBoundingClientRect().top - field.clientTop - (ball.clientHeight / 2)) + 'px'
+    ball.style.left = (event.clientX - field.getBoundingClientRect().left - field.clientLeft - (ball.clientHeight / 2)) + 'px'
+
+    if (event.clientX > field.offsetWidth - field.clientLeft - (ball.clientHeight / 2)) {
+        ball.style.left = 160 + 'px'
     }
 
     if (event.clientX < 38) {
-        ball.style.left = 20 + 'px'
+        ball.style.left = 0 + 'px'
     }
 
 
     if (event.clientY > 186) {
-        ball.style.top = 206 - 76 + 'px'
+        ball.style.top = 206 - 56 + 'px'
     }
 
     if (event.clientY < 78) {
-        ball.style.top = 20 + 'px'
+        ball.style.top = 0 + 'px'
     }
 }
